e16ecbe505d7dd88a827f2a883cf437619958021
Marianne <m.salamanca21@live.ca>
2021-12-13 18:49:53 -0500

    fix router

--- mongoose-flights/app.js
+++ mongoose-flights/app.js
@@ -26,8 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/flights', flightsRouter);
+app.use('/tickets', ticketsRouter);
 app.use('/', destinationsRouter);
-app.use('/tickets/:id', ticketsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- mongoose-flights/routes/tickets.js
+++ mongoose-flights/routes/tickets.js
@@ -2,8 +2,8 @@ let express = require('express');
 let router = express.Router();
 let ticketsCtrl = require('../controllers/tickets');
 	
-router.get('/tickets/new', ticketsCtrl.new);
-router.post('/tickets/:id', ticketsCtrl.create);
-
+router.get('/:id/new', ticketsCtrl.new);
+router.post('/:id', ticketsCtrl.create);
+// don't need to add tickets again because it's already in the router
 	
 module.exports = router;
